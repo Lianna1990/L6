8756b21150e61f744f8d189486ca00968e15ab39
Lianna1990 <egoian.lianna@mail.ru>
2019-03-18 14:28:59 +0300

    homework 6

--- src/Race.java
+++ src/Race.java
@@ -3,8 +3,12 @@ import java.util.Arrays;
 
 public class Race {
     private ArrayList<Stage> stages;
-    public ArrayList<Stage> getStages() { return stages; }
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
     public Race(Stage... stages) {
-        this.stages = new ArrayList<> (Arrays.asList(stages));
+        this.stages = new ArrayList<> (Arrays.asList (stages));
     }
 }
